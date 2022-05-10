b7aa74bd7922bc1ad32f79034004149ce2cbcfcb
Benjamin Wunderling <benjamin.wunderling@student.tugraz.at>
2022-05-10 17:09:31 +0200

    started work implenting

---
+++ src/IPSEC_IKEv1_SUL.py
@@ -0,0 +1,45 @@
+from aalpy.base import SUL
+from aalpy.oracles import RandomWalkEqOracle
+from aalpy.learning_algs import run_Lstar
+from aalpy.utils import visualize_automaton
+
+from IPSEC_Mapper import IPSEC_Mapper
+
+class IPSEC_IKEv1_SUL(SUL):
+    def __init__(self):
+        super().__init__()
+        self.ipsec = IPSEC_Mapper() # TODO
+
+    def pre(self):
+        self.ipsec.state = 'DISCONNECTED'
+
+    def post(self):
+        pass
+    
+    # map to concrete implementation
+    def step(self, letter):
+        if letter == 'sa_main':
+            return self.ipsec.sa_main()
+        elif letter == 'key_ex_main':
+            return self.ipsec.key_ex_main()
+        elif letter == 'authenticate':
+            return self.ipsec.authenticate()
+        elif letter == 'sa_quick':
+            return self.ipsec.sa_quick()
+        elif letter == 'ack_quick':
+            return self.ipsec.ack_quick()
+        elif letter == 'informational':
+            return self.ipsec.informational()
+        else:
+            pass
+        
+
+sul = IPSEC_IKEv1_SUL()
+input_al = ['sa_main', 'key_ex_main', 'authenticate', 'sa_quick', 'ack_quick', 'informational']
+
+eq_oracle = RandomWalkEqOracle(input_al, sul, num_steps=2000, reset_after_cex=True, reset_prob=0.15)
+
+learned_mqtt= run_Lstar(input_al, sul, eq_oracle=eq_oracle, automaton_type='mealy', cache_and_non_det_check=True,
+                  print_level=2)
+
+visualize_automaton(learned_mqtt)
---
+++ src/IPSEC_Mapper.py
@@ -0,0 +1,51 @@
+# TODO: in here, implement the actual calls to the server, that the high level abstraction maps to
+# start with good known basic values and later build up to randomly chosen possible values and finally actual fuzzing
+
+# TODO: decrypt the wireshark ikev1 dumps using key from logs
+
+# Possible states for client: DISCONNECTED --> P1_SA --> P1_DH --> P1_AUTH --> P2_SA --> CONNECTED
+from scapy.layers.inet import IP, UDP
+from scapy.layers.isakmp import ISAKMP, ISAKMP_payload_SA, ISAKMP_payload_Transform
+from scapy.utils import randstring
+
+
+class IPSEC_Mapper:
+    def __init__(self):
+        self.state = 'DISCONNECTED'
+        self.src_ip = "10.0.2.2"  # initiator
+        self.dst_ip = "10.0.2.1"  # responder
+
+    def sa_main(self):
+        # attempt to agree on security params.
+        # Send suggestion --> parse response: agree -> P1_SA, else -> DISCONNECTED
+        # create an ISAKMP packet with scapy:
+        p = IP(src=self.src_ip, dst=self.dst_ip)/UDP(sport=500, dport=500)/ISAKMP(init_cookie=b"\xa2\xee\x50\xbb\x20\xdf\x2d\xfd", next_payload=1, exch_type=2)/ISAKMP_payload_SA(prop=ISAKMP_payload_Transform(
+            transforms=[('Encryption', 'AES-CBC'), ('Hash', 'SHA'), ('Authentication', 'PSK'), ('GroupDesc', '1024MODPgr'), ('KeyLength', 256), ('LifeType', 'Seconds'), ('LifeDuration', 28800)]))
+        p.show()
+        
+        self.state = 'P1_SA'
+        return self.state
+
+    def key_ex_main(self):
+        self.state = 'P1_DH'
+        return self.state
+
+    def authenticate(self):
+        self.state = 'P1_AUTH'
+        return self.state
+
+    def sa_quick(self):
+        self.state = 'P2_SA'
+        return self.state
+
+    def ack_quick(self):
+        self.state = 'CONNECTED'
+        return self.state
+
+    def informational(self):
+        self.state = 'CONNECTED'
+        return self.state
+
+
+ipsm = IPSEC_Mapper()
+ipsm.sa_main()
